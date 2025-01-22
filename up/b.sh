b2e0193545f1738712e3d642ccd3c9bab4e0128a
Aria Fatah <ariafatah0711@gmail.com>
2025-01-22 22:58:51 +0700

    update

--- up/b.sh
+++ up/b.sh
@@ -1,54 +1,15 @@
-# up asj
-## automation setup up
-```bash
-wget https://raw.githubusercontent.com/ariafatah0711/linux_aria/refs/heads/main/up/a.sh | sh
-wget https://raw.githubusercontent.com/ariafatah0711/linux_aria/refs/heads/main/up/b.sh | sh
-```
-
-## run
-```bash
-# setup
-yum install podman haproxy named
+# bind
+yum install named
 firewall-cmd --add-port={22/tcp,53/tcp,53/udp,80tcp,443/tcp,8080-8090/tcp} --permanent
 firewall-cmd --add-service=dns --permanent
 firewall-cmd --reload
 
-# podman
-cd /home/aria
-mkdir web1 web2 && echo "web1" > web1/index.html && echo "web2" > web2/index.html
-
-podman run -d --name web1 -p 8081:80 -v ./web1:/usr/share/nginx/html:Z nginx
-podman run -d --name web2 -p 8082:80 -v ./web2:/usr/share/nginx/html:Z nginx
-
-# haproxy
-cd /etc/haproxy/
-
-cat >> haproxy.cfg << EOF
-frontend fe
-    bind *:80
-    default_backend be
-backend be
-    balance roundrobin
-    server node1 localhost:8081
-    server node2 localhost:8082
-EOF
-
-systemctl enable --now haproxy
-systemctl start haproxy
-systemctl restart haproxy
-
-## haproxy selinux
-ps -eZ | grep haproxy
-semanage fcontext -l | grep haproxy
-semanage permissive -a haproxy_t
-
-# bind
 systemctl enable --now named
 vi /etc/named.conf
 ##
-listen-on port 53 { 127.0.0.1; 11.11.11.1; };
----
-allow-query     { localhost; 0.0.0.0/0; };
+## listen-on port 53 { 127.0.0.1; 11.11.11.1; };
+## ---
+## allow-query     { localhost; 0.0.0.0/0; };
 ##
 
 cd /var/named
@@ -137,4 +98,3 @@ systemctl restart named
 
 nslookup ariafatah.id
 dig @11.11.11.1 ariafatah.id
-```
